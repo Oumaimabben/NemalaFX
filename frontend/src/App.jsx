9d58deeaad28d4d0e1eba9f1c02a38c6c9ba293a
Oumaimabben <obenothmen865@gmail.com>
2025-01-02 02:54:08 +0100

    install tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
+      <h1 className="text-3xl font-bold text-red-600">
         Hello, Tailwind CSS + React!
       </h1>
     </div>
